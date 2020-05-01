ac4bb94cd14f2fdbd1916aefc60570169a63edd4
Sourabh Grover <sourabhgrover29@gmail.com>
2020-05-01 20:47:05 +0530

    Added Back to home page redirection

--- src/components/Chat/ChatHeader.js
+++ src/components/Chat/ChatHeader.js
@@ -1,4 +1,5 @@
-import React, { memo, useState } from "react";
+import React, { useState } from "react";
+import { useHistory } from "react-router-dom";
 import styled from "styled-components";
 import { Navbar, NavbarToggler, Collapse, Nav, NavItem } from "reactstrap";
 import { FaClipboardList, FaSignOutAlt } from "react-icons/fa";
@@ -30,39 +31,41 @@ const LogoText = styled.h1`
 `;
 
 const ChatHeader = () => {
+  const history = useHistory();
 
-    const [isOpen, setIsOpen] = useState(false);
+  const [isOpen, setIsOpen] = useState(false);
 
-    const toggle = () => setIsOpen(!isOpen);
-    return (
-        <NavbarStyled expand="md" className="px-5">
-            <LogoContainer href="/">
-                <LogoImage src={LogoImg} alt="Lens!" />
-                <LogoText className="text-white">Lens!</LogoText>
-            </LogoContainer>
-            <NavbarToggler onClick={toggle} />
-            <Collapse isOpen={isOpen} navbar>
-                <Nav className="ml-auto" navbar>
-                    <NavItem className="mx-2">
-                        <NavButton
-                            text="Summary Details"
-                            icon={<FaClipboardList />}
-                            color="#A1373F"
-                            size="20px"
-                        />
-                    </NavItem>
-                    <NavItem className="mx-2">
-                        <NavButton
-                            text="Sign Out"
-                            icon={<FaSignOutAlt />}
-                            color="#A1373F"
-                            size="20px"
-                        />
-                    </NavItem>
-                </Nav>
-            </Collapse>
-        </NavbarStyled>
-    );
-}
+  const toggle = () => setIsOpen(!isOpen);
+  return (
+    <NavbarStyled expand="md" className="px-5">
+      <LogoContainer href="javascript:void(0)" onClick={() => history.push("/")}>
+        <LogoImage src={LogoImg} alt="Lens!" />
+        <LogoText className="text-white">Lens!</LogoText>
+      </LogoContainer>
+      <NavbarToggler onClick={toggle} />
+      <Collapse isOpen={isOpen} navbar>
+        <Nav className="ml-auto" navbar>
+          <NavItem className="mx-2">
+            <NavButton
+              text="Summary Details"
+              icon={<FaClipboardList />}
+              color="#A1373F"
+              size="20px"
+              onClick={() => history.push("/summary")}
+            />
+          </NavItem>
+          <NavItem className="mx-2">
+            <NavButton
+              text="Sign Out"
+              icon={<FaSignOutAlt />}
+              color="#A1373F"
+              size="20px"
+            />
+          </NavItem>
+        </Nav>
+      </Collapse>
+    </NavbarStyled>
+  );
+};
 
-export default memo(ChatHeader);
+export default ChatHeader;
