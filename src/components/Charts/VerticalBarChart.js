93ccb8132461063eb6dd46c6735a02ce75d2bf55
Sourabh Grover <sourabhgrover29@gmail.com>
2020-05-06 01:24:21 +0530

    Developed Vertical Bar Chart

--- src/components/Charts/VerticalBarChart.js
+++ src/components/Charts/VerticalBarChart.js
@@ -6,51 +6,36 @@ import {
 import ChartContainer from "./ChartContainer"
 
 
-const data = [
-    {
-        name: 'Page A', uv: 4000, pv: 2400, amt: 2400,
-    },
-    {
-        name: 'Page B', uv: 3000, pv: 1398, amt: 2210,
-    },
-    {
-        name: 'Page C', uv: 2000, pv: 9800, amt: 2290,
-    },
-    {
-        name: 'Page D', uv: 2780, pv: 3908, amt: 2000,
-    },
-    {
-        name: 'Page E', uv: 1890, pv: 4800, amt: 2181,
-    },
-    {
-        name: 'Page F', uv: 2390, pv: 3800, amt: 2500,
-    },
-    {
-        name: 'Page G', uv: 3490, pv: 4300, amt: 2100,
-    },
-];
 
 export default class VerticalBarChart extends PureComponent {
 
     render() {
+
+        var graphData = []
+        this.props.values.map((v) => {
+
+            graphData.push({
+                name: Object.keys(v)[0], value: Number(Object.values(v)[0])
+            });
+        })
         return (
             <ResponsiveContainer>
                 <ChartContainer title={this.props['name:']} >
                     <BarChart
                         width={500}
                         height={300}
-                        data={data}
+                        data={graphData}
                         margin={{
                             top: 5, right: 30, left: 20, bottom: 5,
                         }}
                     >
-                        <CartesianGrid strokeDasharray="3 3" />
+
                         <XAxis dataKey="name" />
                         <YAxis />
                         <Tooltip />
                         <Legend />
-                        <Bar dataKey="pv" fill="#8884d8" />
-                        <Bar dataKey="uv" fill="#82ca9d" />
+                        <Bar dataKey="value" fill="#8884d8" />
+
                     </BarChart>
                 </ChartContainer>
             </ResponsiveContainer>
